85937ee4f067318639bf590868cc0f15f3aad02e
José Guilherme <guilhermeverissimo14@gmail.com>
2022-11-07 17:05:06 -0300

    Criando menu lateral

--- Sistema-de-gestão-de-controle/index.php
+++ Sistema-de-gestão-de-controle/index.php
@@ -20,16 +20,41 @@
 
 <body>
     <header>
-        <a href="#" class="btn-abrir" onclick="abrirMenu()"> &#9776;</a>
-        <h2>Sistema de gestão de controle</h2>
+        <div>
+            <a href="#" class="btn-abrir" onclick="abrirMenu()"> &#9776;</a>
+        </div>
+        <div>
+            <h2>Sistema de gestão de controle</h2>
+        </div>
     </header>
     <nav id="menu">
-        <a href="#" onclick="fecharMenu()">&times;</a>
-        <a href="#">Vendas</a>
-        <a href="#">Compras</a>
-        <a href="#">Usuarios</a>
-        <a href="#">Relatorios</a>
-        <a href="#">Produtos</a>
+        <a style="font-size: 50px;" href="#" onclick="fecharMenu()">&times;</a>
+        <div class="nav-1">
+            <img src="../Sistema-de-gestão-de-controle/assets/images/logo.png" alt="logo">
+                <a href="#" class="itens">
+                    <img src="../Sistema-de-gestão-de-controle/assets/images/vendas.png" alt="vendas">
+                    Vendas
+                </a>
+
+                <a href="#" class="itens">
+                    <img src="../Sistema-de-gestão-de-controle/assets/images/compras.png" alt="compras">
+                    Compras
+                </a>
+
+                <a href="#" class="itens">
+                    <img src="../Sistema-de-gestão-de-controle/assets/images/usuarios.png" alt="usuarios">
+                    Usuarios
+                </a>
+
+                <a href="#" class="itens">
+                    <img src="../Sistema-de-gestão-de-controle/assets/images/relatorio.png" alt="relatorios">
+                    Relatorios
+                </a>
+                <a href="#" class="itens">
+                    <img src="../Sistema-de-gestão-de-controle/assets/images/produtos.png" alt="produtos">
+                    Produtos
+                </a>
+        </div>
     </nav>
 
 </body>
