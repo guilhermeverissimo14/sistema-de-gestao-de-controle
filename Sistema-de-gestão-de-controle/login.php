d1e9fedc322f0e4f7edf60e2ff1fe8885fe973b8
José Guilherme <guilhermeverissimo14@gmail.com>
2022-11-16 06:21:57 -0300

    Adicionando tela de login

--- Sistema-de-gestão-de-controle/login.php
+++ Sistema-de-gestão-de-controle/login.php
@@ -1,16 +1,34 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="./assets/css/login.css">
 </head>
+
 <body>
- <form method="POST" action="loginAction.php">
-    <input type="text" name="email"/><br>
-    <input type="password" name="pass"/><br>
-    <button>Enviar</button>
- </form>  
+  <div class="container right-panel-active">
+    <!-- Entrar -->
+    <div class="container__form container--signup">
+      <form class="form" id="form1" method="POST" action="loginAction.php">
+        <h2 class="form__title">Login</h2>
+        <input type="text"  placeholder="Email" name="email" class="input" required/>
+        <input type="password" placeholder="Senha" name="pass" class="input" required>
+        <button class="btn">Entrar</button>
+      </form>
+    </div>
+    
+    <!-- sobreposição -->
+    <div class="container__overlay">
+      <div class="overlay">
+        <img src="../Sistema-de-gestão-de-controle/assets/images/logo3.png" >
+      </div>
+    </div>
+  </div>
 </body>
+
+</html>
 </html>
