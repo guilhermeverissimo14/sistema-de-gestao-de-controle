3337332ab95c382371fb5c444d330af292e8718c
José Guilherme <guilhermeverissimo14@gmail.com>
2022-11-17 15:17:33 -0300

    adicionando tela de cadastro para vendas

--- Sistema-de-gestão-de-controle/editar.php
+++ Sistema-de-gestão-de-controle/editar.php
@@ -15,19 +15,54 @@ $venda = $vendaDao->get($id);
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="./assets/css/vendas.css">
 </head>
 
 <body>
-    <form method="POST" action="editarAction.php">
-        <input type="hidden" name="id" value="<?=$venda['id'] ?>" />
-        <input type="date" value="<?= $venda['date'] ?>" placeholder="date" name="date" />
-        <input type="text" value="<?= $venda['total'] ?>" placeholder="Valor Total" name="total" />
-        <input type="text" value="<?= $venda['product'] ?>" placeholder="Nome do produto" name="produto" />
-        <input type="text" value="<?= $venda['seller'] ?>" placeholder="Nome Vendedor" name="vendedor" />
-        <input type="number" value="<?= $venda['amount'] ?>" placeholder="Quantidade" name="Quantidade" />
-        <input type="text" value="<?= $venda['status'] ?>" placeholder="status" name="status" />
-        <button>Enviar</button>
-    </form>
+<div class="box">
+    <div class="img-box" style="border: 1px solid ;">
+      <img src="./assets/images/logo3.png">
+    </div>
+    <div class="form-box" style="border: 1px solid;">
+      <h2>Cadastro de Vendas</h2>
+      <form action="#">
+        <div class="input-group">
+          <label for="nome"> Data da venda</label>
+          <input type="date" value="<?= $venda['date'] ?>" placeholder="data" name="date" />
+        </div>
+
+        <div class="input-group">
+          <label for="email">Valor Total</label>
+          <input type="text" value="<?= $venda['total'] ?>" placeholder="Valor Total" name="total" />
+        </div>
+
+        <div class="input-group">
+          <label for="telefone">Nome do produto</label>
+          <input type="text" value="<?= $venda['product'] ?>" placeholder="Nome do produto" name="produto" />
+        </div>
+
+        <div class="input-group">
+          <label for="telefone">Nome do vendedor</label>
+          <input type="text" value="<?= $venda['seller'] ?>" placeholder="Nome Vendedor" name="vendedor" />
+        </div>
+        
+        <div class="input-group">
+          <label for="telefone">Quantidade</label>
+          <input type="number" value="<?= $venda['amount'] ?>" placeholder="Quantidade" name="Quantidade" />
+        </div>
+
+        <div class="input-group">
+          <label for="telefone">Status</label>
+          <input type="text" value="<?= $venda['status'] ?>" placeholder="status" name="status" />
+        </div>
+
+        <div class="input-group">
+          <button>Enviar</button>
+        </div>
+
+      </form>
+    </div>
+  </div>
 </body>
 
 </html>
