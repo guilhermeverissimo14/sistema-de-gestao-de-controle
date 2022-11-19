0ee30146da2302ed68f851d071ab4688ebfa5fa8
José Guilherme <guilhermeverissimo14@gmail.com>
2022-11-19 12:34:45 -0300

    Alinhano itens da tabela no centro

--- Sistema-de-gestão-de-controle/conexao.php
+++ Sistema-de-gestão-de-controle/conexao.php
@@ -1,6 +1,6 @@
 <?php
 try {
-    $conexao = new PDO("mysql:dbname=stock_management;host=localhost","root","mysql");
+    $conexao = new PDO("mysql:dbname=stock_management;host=localhost","root","");
 }catch(PDOException $e){
     echo 'Deu ruim '.$e->getMessage();
 }
