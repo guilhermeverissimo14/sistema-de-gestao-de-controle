658d599d5591c29dec24bc2332147e90603f68d4
José Guilherme <guilhermeverissimo14@gmail.com>
2022-11-28 23:20:22 -0300

    Finalizando a estilização da tabela

--- Sistema-de-gestão-de-controle/index.php
+++ Sistema-de-gestão-de-controle/index.php
@@ -97,7 +97,7 @@ require_once 'dao/DaoUsuarios.php';
     <div class="container">
         <div id="tb-vendas">
             <h1>Tabela de vendas</h1>
-            <a class="adicionar-vendas" href="adicionarVendas.php" style="background-color: green;">Adicionar Vendas</a>
+            <a class="adicionar" href="adicionarVendas.php">Adicionar</a>
             <table>
                 <tr>
                     <th>id</th>
@@ -120,14 +120,14 @@ require_once 'dao/DaoUsuarios.php';
                         <td><?= $item->getTotal() ?></td>
                         <td><?= $item->getStatus() ?></td>
                         <td>
-                            <a style="background-color:yellow;" href="editarVendas.php?id=<?php echo $item->getId() ?>">
+                            <a style="background-color:yellow; padding:5px;" href="editarVendas.php?id=<?php echo $item->getId() ?>">
                                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="black" class="bi bi-pencil" viewBox="0 0 16 16">
                                     <path d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168l10-10zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207 11.207 2.5zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293l6.5-6.5zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325z" />
                                 </svg>
                             </a>
                         </td>
                         <td>
-                            <a style="background-color:red;" href="ExcluirVendas.php?id=<?php echo $item->getId() ?>">
+                            <a style="background-color:red; padding:5px;" href="ExcluirVendas.php?id=<?php echo $item->getId() ?>">
                                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="black" class="bi bi-trash3-fill" viewBox="0 0 16 16">
                                     <path d="M11 1.5v1h3.5a.5.5 0 0 1 0 1h-.538l-.853 10.66A2 2 0 0 1 11.115 16h-6.23a2 2 0 0 1-1.994-1.84L2.038 3.5H1.5a.5.5 0 0 1 0-1H5v-1A1.5 1.5 0 0 1 6.5 0h3A1.5 1.5 0 0 1 11 1.5Zm-5 0v1h4v-1a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5ZM4.5 5.029l.5 8.5a.5.5 0 1 0 .998-.06l-.5-8.5a.5.5 0 1 0-.998.06Zm6.53-.528a.5.5 0 0 0-.528.47l-.5 8.5a.5.5 0 0 0 .998.058l.5-8.5a.5.5 0 0 0-.47-.528ZM8 4.5a.5.5 0 0 0-.5.5v8.5a.5.5 0 0 0 1 0V5a.5.5 0 0 0-.5-.5Z" />
                                 </svg>
@@ -146,7 +146,7 @@ require_once 'dao/DaoUsuarios.php';
         <!-- tabela de compras -->
         <div id="tb-compras">
             <h1>Tabela de compras</h1>
-            <a class="adicionar-compras" href="adicionarCompras.php" style="background-color: green;">Adicionar Compras</a>
+            <a class="adicionar" href="adicionarCompras.php">Adicionar</a>
             <table>
                 <tr>
                     <th>Id</th>
@@ -162,14 +162,14 @@ require_once 'dao/DaoUsuarios.php';
                         <td><?= $item->getNome() ?></td>
                         <td><?= $item->getValor() ?></td>
                         <td>
-                            <a style="background-color:yellow;" href="editarCompras.php?id=<?php echo $item->getId() ?>">
+                            <a style="background-color:yellow; padding:5px;" href="editarCompras.php?id=<?php echo $item->getId() ?>">
                                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="black" class="bi bi-pencil" viewBox="0 0 16 16">
                                     <path d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168l10-10zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207 11.207 2.5zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293l6.5-6.5zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325z" />
                                 </svg>
                             </a>
                         </td>
                         <td>
-                            <a style="background-color:red ;" href="ExcluirCompras.php?id=<?php echo $item->getId() ?>">
+                            <a style="background-color:red; padding:5px;" href="ExcluirCompras.php?id=<?php echo $item->getId() ?>">
                                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="black" class="bi bi-trash3-fill" viewBox="0 0 16 16">
                                     <path d="M11 1.5v1h3.5a.5.5 0 0 1 0 1h-.538l-.853 10.66A2 2 0 0 1 11.115 16h-6.23a2 2 0 0 1-1.994-1.84L2.038 3.5H1.5a.5.5 0 0 1 0-1H5v-1A1.5 1.5 0 0 1 6.5 0h3A1.5 1.5 0 0 1 11 1.5Zm-5 0v1h4v-1a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5ZM4.5 5.029l.5 8.5a.5.5 0 1 0 .998-.06l-.5-8.5a.5.5 0 1 0-.998.06Zm6.53-.528a.5.5 0 0 0-.528.47l-.5 8.5a.5.5 0 0 0 .998.058l.5-8.5a.5.5 0 0 0-.47-.528ZM8 4.5a.5.5 0 0 0-.5.5v8.5a.5.5 0 0 0 1 0V5a.5.5 0 0 0-.5-.5Z" />
                                 </svg>
@@ -188,7 +188,7 @@ require_once 'dao/DaoUsuarios.php';
         <!-- tabela de usuarios -->
         <div id="tb-usuario">
             <h1>Tabela de Usuários</h1>
-            <a class="adicionar-usuarios" href="adicionarUsuarios.php" style="background-color: green;">Adicionar Usuarios</a>
+            <a class="adicionar" href="adicionarUsuarios.php">Adicionar</a>
             <table>
                 <tr>
                     <th>Id</th>
@@ -207,14 +207,14 @@ require_once 'dao/DaoUsuarios.php';
                         <td><?= $item->getSenha() ?></td>
                         <td><?= $item->getAcesso() ?></td>
                         <td>
-                            <a style="background-color:yellow;" href="editarUsuarios.php?id=<?php echo $item->getId() ?>">
+                            <a style="background-color:yellow; padding:5px;" href="editarUsuarios.php?id=<?php echo $item->getId() ?>">
                                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="black" class="bi bi-pencil" viewBox="0 0 16 16">
                                     <path d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168l10-10zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207 11.207 2.5zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293l6.5-6.5zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325z" />
                                 </svg>
                             </a>
                         </td>
                         <td>
-                            <a style="background-color:red ;" href="ExcluirUsuarios.php?id=<?php echo $item->getId() ?>">
+                            <a style="background-color:red; padding:5px;" href="ExcluirUsuarios.php?id=<?php echo $item->getId() ?>">
                                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="black" class="bi bi-trash3-fill" viewBox="0 0 16 16">
                                     <path d="M11 1.5v1h3.5a.5.5 0 0 1 0 1h-.538l-.853 10.66A2 2 0 0 1 11.115 16h-6.23a2 2 0 0 1-1.994-1.84L2.038 3.5H1.5a.5.5 0 0 1 0-1H5v-1A1.5 1.5 0 0 1 6.5 0h3A1.5 1.5 0 0 1 11 1.5Zm-5 0v1h4v-1a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5ZM4.5 5.029l.5 8.5a.5.5 0 1 0 .998-.06l-.5-8.5a.5.5 0 1 0-.998.06Zm6.53-.528a.5.5 0 0 0-.528.47l-.5 8.5a.5.5 0 0 0 .998.058l.5-8.5a.5.5 0 0 0-.47-.528ZM8 4.5a.5.5 0 0 0-.5.5v8.5a.5.5 0 0 0 1 0V5a.5.5 0 0 0-.5-.5Z" />
                                 </svg>
@@ -233,7 +233,7 @@ require_once 'dao/DaoUsuarios.php';
         <!-- tabela de prudutos -->
         <div id="tb-produto">
             <h1>Tabela de produtos</h1>
-            <a class="adicionar-produtos" href="adicionarProdutos.php" style="background-color: green;">Adicionar Produtos</a>
+            <a class="adicionar" href="adicionarProdutos.php">Adicionar</a>
             <table>
                 <tr>
                     <th>Id</th>
@@ -249,14 +249,14 @@ require_once 'dao/DaoUsuarios.php';
                         <td><?= $item->getNome() ?></td>
                         <td><?= $item->getValor() ?></td>
                         <td>
-                            <a style="background-color:yellow ;" href="editarProdutos.php?id=<?php echo $item->getId() ?>">
+                            <a style="background-color:yellow; padding:5px;" href="editarProdutos.php?id=<?php echo $item->getId() ?>">
                                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="black" class="bi bi-pencil" viewBox="0 0 16 16">
                                     <path d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168l10-10zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207 11.207 2.5zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293l6.5-6.5zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325z" />
                                 </svg>
                             </a>
                         </td>
                         <td>
-                            <a style="background-color:red ;" href="ExcluirProdutos.php?id=<?php echo $item->getId() ?>">
+                            <a style="background-color:red; padding:5px;" href="ExcluirProdutos.php?id=<?php echo $item->getId() ?>">
                                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="black" class="bi bi-trash3-fill" viewBox="0 0 16 16">
                                     <path d="M11 1.5v1h3.5a.5.5 0 0 1 0 1h-.538l-.853 10.66A2 2 0 0 1 11.115 16h-6.23a2 2 0 0 1-1.994-1.84L2.038 3.5H1.5a.5.5 0 0 1 0-1H5v-1A1.5 1.5 0 0 1 6.5 0h3A1.5 1.5 0 0 1 11 1.5Zm-5 0v1h4v-1a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5ZM4.5 5.029l.5 8.5a.5.5 0 1 0 .998-.06l-.5-8.5a.5.5 0 1 0-.998.06Zm6.53-.528a.5.5 0 0 0-.528.47l-.5 8.5a.5.5 0 0 0 .998.058l.5-8.5a.5.5 0 0 0-.47-.528ZM8 4.5a.5.5 0 0 0-.5.5v8.5a.5.5 0 0 0 1 0V5a.5.5 0 0 0-.5-.5Z" />
                                 </svg>
