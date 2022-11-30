bc968ed1c764954002262089d6dd601b9ad16013
José Guilherme <guilhermeverissimo14@gmail.com>
2022-11-30 10:25:56 -0300

    Estilizando botão de sair

--- Sistema-de-gestão-de-controle/index.php
+++ Sistema-de-gestão-de-controle/index.php
@@ -6,7 +6,7 @@ require_once 'dao/DaoCompras.php';
 require_once 'dao/DaoProdutos.php';
 require_once 'dao/DaoUsuarios.php';
 
-if(empty($_SESSION['token'])){
+if (empty($_SESSION['token'])) {
     header("location:login.php");
     exit;
 }
@@ -36,21 +36,17 @@ $usuarioLogado = $usuarioDao->autenticacao($_SESSION['token']);
 <script src="./assets/js/home.js">
 
 </script>
+
 <body>
     <header>
         <a href="#" class="btn-abrir" onclick="abrirMenu()">&#9776;</a>
-        <a href="sair.php" onClick="return confirm('Você tem certeza?');">Sair
-            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-backspace" viewBox="0 0 16 16">
-                <path d="M5.83 5.146a.5.5 0 0 0 0 .708L7.975 8l-2.147 2.146a.5.5 0 0 0 .707.708l2.147-2.147 2.146 2.147a.5.5 0 0 0 .707-.708L9.39 8l2.146-2.146a.5.5 0 0 0-.707-.708L8.683 7.293 6.536 5.146a.5.5 0 0 0-.707 0z" />
-                <path d="M13.683 1a2 2 0 0 1 2 2v10a2 2 0 0 1-2 2h-7.08a2 2 0 0 1-1.519-.698L.241 8.65a1 1 0 0 1 0-1.302L5.084 1.7A2 2 0 0 1 6.603 1h7.08zm-7.08 1a1 1 0 0 0-.76.35L1 8l4.844 5.65a1 1 0 0 0 .759.35h7.08a1 1 0 0 0 1-1V3a1 1 0 0 0-1-1h-7.08z" />
-            </svg>
-
-        </a>
         <h2>Sistema de gestão de controle </h2>
+        <a href="sair.php" onClick="return confirm('Você tem certeza?');">
+            <img style="width:10%; margin-right:-7em;" src="../Sistema-de-gestão-de-controle/assets/images/sair.png" alt="sair">
+        </a>
         <!-- //$usuarioLogado['acess'] -->
-
     </header>
-    
+
     <!-- logo da loja -->
     <div id="img-logo" class="img">
         <img src="../Sistema-de-gestão-de-controle/assets/images/logo4.png" alt="logo">
@@ -81,7 +77,7 @@ $usuarioLogado = $usuarioDao->autenticacao($_SESSION['token']);
                     Usuarios
                 </a>
 
-                <a href="#" class="itens bloquear">
+                <a href="#" onclick="tabelaRelatorios()" class="itens bloquear">
                     <img src="../Sistema-de-gestão-de-controle/assets/images/relatorio.png" alt="relatorios">
                     Relatorios
                 </a>
@@ -169,7 +165,7 @@ $usuarioLogado = $usuarioDao->autenticacao($_SESSION['token']);
                         <td><?= $item->getNome() ?></td>
                         <td><?= $item->getValor() ?></td>
                         <td>
-                            <a  style="background-color:yellow; border-radius:20px; padding:5px;" href="editarCompras.php?id=<?php echo $item->getId() ?>">
+                            <a style="background-color:yellow; border-radius:20px; padding:5px;" href="editarCompras.php?id=<?php echo $item->getId() ?>">
                                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="black" class="bi bi-pencil" viewBox="0 0 16 16">
                                     <path d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168l10-10zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207 11.207 2.5zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293l6.5-6.5zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325z" />
                                 </svg>
@@ -214,7 +210,7 @@ $usuarioLogado = $usuarioDao->autenticacao($_SESSION['token']);
                         <td><?= $item->getSenha() ?></td>
                         <td><?= $item->getAcesso() ?></td>
                         <td>
-                            <a  style="background-color:yellow; border-radius:20px; padding:5px;" href="editarUsuarios.php?id=<?php echo $item->getId() ?>">
+                            <a style="background-color:yellow; border-radius:20px; padding:5px;" href="editarUsuarios.php?id=<?php echo $item->getId() ?>">
                                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="black" class="bi bi-pencil" viewBox="0 0 16 16">
                                     <path d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168l10-10zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207 11.207 2.5zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293l6.5-6.5zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325z" />
                                 </svg>
@@ -256,7 +252,7 @@ $usuarioLogado = $usuarioDao->autenticacao($_SESSION['token']);
                         <td><?= $item->getNome() ?></td>
                         <td><?= $item->getValor() ?></td>
                         <td>
-                            <a  style="background-color:yellow; border-radius:20px; padding:5px;" href="editarProdutos.php?id=<?php echo $item->getId() ?>">
+                            <a style="background-color:yellow; border-radius:20px; padding:5px;" href="editarProdutos.php?id=<?php echo $item->getId() ?>">
                                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="black" class="bi bi-pencil" viewBox="0 0 16 16">
                                     <path d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168l10-10zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207 11.207 2.5zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293l6.5-6.5zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325z" />
                                 </svg>
@@ -273,11 +269,14 @@ $usuarioLogado = $usuarioDao->autenticacao($_SESSION['token']);
                 <?php endforeach; ?>
             </table>
         </div>
+
+        <!-- Tabela de relatorios -->
         <div id="tb-relatorios">
+            <h1>Tabela de Relatorios</h1>
             <table>
                 <tr>
                     <th>Quantidade</th>
-                    <th>Nome</th>
+                    <th>Nome Vendedor</th>
                     <th>Valor</th>
                     <th colspan="2">Ações</th>
                 </tr>
@@ -285,16 +284,15 @@ $usuarioLogado = $usuarioDao->autenticacao($_SESSION['token']);
                     <td></td>
                     <td></td>
                     <td></td>
-                    <td></td>
                     <td>
-                        <a>
+                        <a style="background-color:yellow; border-radius:20px; padding:5px;">
                             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="black" class="bi bi-pencil" viewBox="0 0 16 16">
                                 <path d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168l10-10zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207 11.207 2.5zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293l6.5-6.5zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325z" />
                             </svg>
                         </a>
                     </td>
                     <td>
-                        <a>
+                        <a style="background-color:blue; border-radius:20px; padding:5px;">
                             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="white" class="bi bi-trash3-fill" viewBox="0 0 16 16">
                                 <path d="M11 1.5v1h3.5a.5.5 0 0 1 0 1h-.538l-.853 10.66A2 2 0 0 1 11.115 16h-6.23a2 2 0 0 1-1.994-1.84L2.038 3.5H1.5a.5.5 0 0 1 0-1H5v-1A1.5 1.5 0 0 1 6.5 0h3A1.5 1.5 0 0 1 11 1.5Zm-5 0v1h4v-1a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5ZM4.5 5.029l.5 8.5a.5.5 0 1 0 .998-.06l-.5-8.5a.5.5 0 1 0-.998.06Zm6.53-.528a.5.5 0 0 0-.528.47l-.5 8.5a.5.5 0 0 0 .998.058l.5-8.5a.5.5 0 0 0-.47-.528ZM8 4.5a.5.5 0 0 0-.5.5v8.5a.5.5 0 0 0 1 0V5a.5.5 0 0 0-.5-.5Z" />
                             </svg>
@@ -332,4 +330,5 @@ if (isset($_SESSION['flag'])) {
     $_SESSION['flag'] = false;
 }
 ?>
+
 </html>
